c19d62e3fa918037976cbed351f80e50c299ae48
Guilherme Torres <guiatorres8@gmail.com>
2021-10-14 10:47:50 -0300

    finish login module styling

---
+++ src/components/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-card width="400" class="mx-auto mt-16">
+      <v-card-title>
+        <h1 class="display-1">Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field
+            label="User ID"
+            prepend-icon="mdi-account-circle"
+          ></v-text-field>
+          <v-text-field
+            :type="showPassword ? 'text' : 'password'"
+            label="User Password"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="success">Register</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="info">Login</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      showPassword: false,
+    }
+  },
+}
+</script>
+
+<style scoped></style>
